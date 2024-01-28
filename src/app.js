56705bfdcd38636d424c9080dd7e37a0b044bbf5
Alex Santos <alexn.santos18@gmail.com>
2024-01-27 22:44:59 -0500

    Set up project file structure and server connection

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
+const gw2Api = require("./utils/gw2Api");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+//Define paths for express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../template/partials");
+
+//set up hbs as the view engine, set views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+//set up static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "GW2 Raid Tracker",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server up on port ${port}`);
+});
