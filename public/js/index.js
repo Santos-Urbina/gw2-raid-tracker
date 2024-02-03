7322d91e334840228cbfef6840b56ae6f54ffbb1
Alex Santos <alexn.santos18@gmail.com>
2024-02-02 22:45:24 -0500

    Changed index page so that only one set of data is displayed at a time

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,7 @@
-const accountDataButton = document.querySelector("button");
+const accountDataButton = document.querySelector("#accountButton");
 const accountDataText = document.querySelector("#accountDataText");
+const raidDataButton = document.querySelector("#raidsButton");
+const raidDataText = document.querySelector("#raidDataText");
 
 accountDataButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -12,7 +14,34 @@ accountDataButton.addEventListener("click", (e) => {
         console.log("404");
       } else {
         console.log("200");
+
+        if (raidDataText.textContent !== "") {
+          raidDataText.textContent = "";
+        }
+
         accountDataText.textContent = JSON.stringify(data, undefined, 2);
+      }
+    });
+  });
+});
+
+raidDataButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  raidDataText.textContent = "Loading...";
+
+  fetch("/raids").then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        console.log("404");
+      } else {
+        console.log("200");
+
+        if (accountDataText.textContent !== "") {
+          accountDataText.textContent = "";
+        }
+
+        raidDataText.textContent = JSON.stringify(data, undefined, 2);
       }
     });
   });
