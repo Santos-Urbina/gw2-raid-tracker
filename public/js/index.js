e90afcc90c0f3bb16080da0be5959d1b0b348571
Alex Santos <alexn.santos18@gmail.com>
2024-02-02 23:13:33 -0500

    Made it so that buttons can only be clicked once and made it so data on both columns can be displayed at once. Adjusted some styling.

--- public/js/index.js
+++ public/js/index.js
@@ -3,46 +3,46 @@ const accountDataText = document.querySelector("#accountDataText");
 const raidDataButton = document.querySelector("#raidsButton");
 const raidDataText = document.querySelector("#raidDataText");
 
-accountDataButton.addEventListener("click", (e) => {
-  e.preventDefault();
+accountDataButton.addEventListener(
+  "click",
+  (e) => {
+    e.preventDefault();
 
-  accountDataText.textContent = "Loading...";
+    accountDataText.textContent = "Loading...";
 
-  fetch("/account").then((response) => {
-    response.json().then((data) => {
-      if (data.error) {
-        console.log("404");
-      } else {
-        console.log("200");
+    fetch("/account").then((response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log("404");
+        } else {
+          console.log("200");
 
-        if (raidDataText.textContent !== "") {
-          raidDataText.textContent = "";
+          accountDataText.textContent = JSON.stringify(data, undefined, 2);
         }
-
-        accountDataText.textContent = JSON.stringify(data, undefined, 2);
-      }
+      });
     });
-  });
-});
+  },
+  { once: true }
+);
 
-raidDataButton.addEventListener("click", (e) => {
-  e.preventDefault();
+raidDataButton.addEventListener(
+  "click",
+  (e) => {
+    e.preventDefault();
 
-  raidDataText.textContent = "Loading...";
+    raidDataText.textContent = "Loading...";
 
-  fetch("/raids").then((response) => {
-    response.json().then((data) => {
-      if (data.error) {
-        console.log("404");
-      } else {
-        console.log("200");
+    fetch("/raids").then((response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log("404");
+        } else {
+          console.log("200");
 
-        if (accountDataText.textContent !== "") {
-          accountDataText.textContent = "";
+          raidDataText.textContent = JSON.stringify(data, undefined, 2);
         }
-
-        raidDataText.textContent = JSON.stringify(data, undefined, 2);
-      }
+      });
     });
-  });
-});
+  },
+  { once: true }
+);
